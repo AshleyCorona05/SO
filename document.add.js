c63bab2ff5ae390e2c5993b2b95bc1f29fbb2d09
AshleyCorona05 <ashleychicuellar@gmail.com>
2024-06-12 23:35:21 -0600

    Add files via upload

---
+++ document.add.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll("nav ul li a");
+    const sections = document.querySelectorAll("section");
+
+    links.forEach(link => {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            sections.forEach(section => {
+                if (section.getAttribute("id") === targetId) {
+                    section.scrollIntoView({ behavior: "smooth" });
+                }
+            });
+        });
+    });
+});
